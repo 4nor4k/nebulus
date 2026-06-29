2d65ea99e318aef3fa9e97e24e86264721f3a82b
4nor4k <claudiovaz.me@gmail.com>
2026-06-29 11:01:18 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'nebulus-v1';
+const ASSETS = [
+  '/nebulus/nebulus-dashboard.html',
+  '/nebulus/nebulus-clientes.html',
+  '/nebulus/nebulus-historico.html',
+  '/nebulus/nebulus-relatorio.html',
+  '/nebulus/nebulus-catalogo.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Sempre busca da rede primeiro, cai no cache se offline
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
